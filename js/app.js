7c655bab1e6e0671624d94a8826d41da1f804adc
rovermind <zentlekimbba@gmail.com>
2016-09-22 23:16:02 +0900

    블로그 수정

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-(function(){
+(function(global, $){
   'use strict';
 
   new WOW().init();
+
+  $('.section-jump.back').on('click', function(e){
+  	$('html, body').animate({ scrollTop: 0 }, 1400 );
+		e.preventDefault();
+   });
+
+   $('.section-jump.go').on('click', function(e){
+ 		$('html, body').animate({ 
+		scrollTop: $(document).height()-$(window).height()}, 1400 );
+		e.preventDefault();
+   });
   
-})();
+})(this, this.jQuery);
